b207c175c065cb668a813c397d40ad8a4f30b415
francocasafus22 <francocasafus55@gmail.com>
2026-01-12 14:07:46 -0300

    add upload-image base

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,7 @@ import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { CouponsModule } from './coupons/coupons.module';
+import { UploadImageModule } from './upload-image/upload-image.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { CouponsModule } from './coupons/coupons.module';
     ProductsModule,
     TransactionsModule,
     CouponsModule,
+    UploadImageModule,
   ],
   controllers: [AppController],
   providers: [AppService],
